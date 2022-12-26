07e1b3f9e820063261020b272495137cd6d38a8d
leeroymk <nikekursk@gmail.com>
2022-12-26 14:49:46 +0300

    [mod] modified test_main_page.py file

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,9 @@
 from selenium.webdriver.common.by import By
-# from time import sleep
+from .pages.main_page import MainPage
 
-
-# def test_add_to_basket_button(browser):
-#     browser.get(
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-#     button = browser.find_element(
-#         By.CSS_SELECTOR,
-#         "button[class='btn btn-lg btn-primary btn-add-to-basket']")
-#     sleep(10)
-#     assert button, 'No button here'
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
