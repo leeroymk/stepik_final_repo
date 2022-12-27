b013db25f3a73d8dbf402b425fab5e5dfc0c78c2
leeroymk <nikekursk@gmail.com>
2022-12-27 09:33:17 +0300

    [mod] pytest marks are deleted

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,7 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
-import pytest
 
 
-@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -11,7 +9,6 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
 
 
-@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
