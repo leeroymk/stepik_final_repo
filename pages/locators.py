24e1d1da078208e48b8543608a36ddd15e912abf
leeroymk <nikekursk@gmail.com>
2022-12-27 09:30:24 +0300

    [mod][add] 4.2 step 8, modified pages files, test_main_page,py file. added pages/login_page.py file

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    Login_link = (By.CSS_SELECTOR, "#id_login-username")
+    Registration_link_email = (By.CSS_SELECTOR, '#id_registration-email')
